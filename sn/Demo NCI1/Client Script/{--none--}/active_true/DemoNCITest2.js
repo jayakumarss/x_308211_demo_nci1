4af655d9c129d52529905237eb578c1d28f180fd
System Administrator <admin@example.com>
2019-12-12 14:34:45 -0500

    Changes by 'System Administrator' on 'Demo NCI1 - 1.0.0' -- Build #1

---
+++ sn/Demo NCI1/Client Script/{--none--}/active_true/DemoNCITest2.js
@@ -0,0 +1,20 @@
+/* 
+ * Application : Demo NCI1
+ * ClassName   : sys_script_client
+ * Created On  : 2019-12-12 19:32:59
+ * Created By  : admin
+ * Updated On  : 2019-12-12 19:33:05
+ * Updated By  : admin
+ * URL         : /sys_script_client.do?sys_id=a62e207bdb250010343f58b3ca96190c
+ */
+test();
+/**
+ * Description
+ * 
+ * @author System Administrator [admin]
+ * @memberof x_308211_demo_nci1.module:sys_script_client
+ * @returns {undefined}
+ */
+function test1(){
+	alert('test');
+}
